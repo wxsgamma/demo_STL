86a55d53a6b97ca73ac7e19a09f35cd0caa8f610
wxsgamma <1170834390@qq.com>
2023-12-08 14:40:04 +0800

    2

--- STL/Vector.hpp
+++ STL/Vector.hpp
@@ -1,6 +1,6 @@
 #include<iostream>
 
-
+//看一下要不要从实现algo_base开始实现整个STL，还是说单纯实现vector deque unordered_map algorithm等
 template<typename P>
 class Vector{
 public:
--- Test/test1.cpp
+++ Test/test1.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 #include<vector>
 
+//预处理（展开宏定义等） = 》 编译阶段 = 》 汇编 = 》 链接
+//编译 词法分析 = 》 语法分析 = 》语义分析 =》 中间代码生成 =》优化 =》 生成和机器相符合的代码 = 》 优化
+//中间存在符号表
+
+void test(){
+    std::cout<<"Hello g++";
+}
+
 int main(){
-    std::vector<int>test;
+    test();
 }
